a3e2d5c2caac666cbc6819236b4bddcc808c8505
pchchv <ipchchv@gmail.com>
2025-05-20 12:42:11 +0300

    Add cTag type

--- cache.go
+++ cache.go
@@ -21,6 +21,22 @@ const (
 
 type tagType uint8
 
+type cTag struct {
+	tag                  string
+	aliasTag             string
+	actualAliasTag       string
+	param                string
+	keys                 *cTag // only populated when using tag's 'keys' and 'endkeys' for map key validation
+	next                 *cTag
+	fn                   FuncCtx
+	typeof               tagType
+	hasTag               bool
+	hasAlias             bool
+	hasParam             bool // true if parameter used e. g. eq = where the equal sign has been set
+	isBlockEnd           bool // indicates the current tag represents the last validation in the block
+	runValidationWhenNil bool
+}
+
 type structCache struct {
 	lock sync.Mutex
 	m    atomic.Value
