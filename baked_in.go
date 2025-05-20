33652ed2573a7d7bc6a80815c8d18233a6a244e0
pchchv <ipchchv@gmail.com>
2025-05-20 12:35:43 +0300

    Implement a wrapFunc function

--- baked_in.go
+++ baked_in.go
@@ -1,5 +1,7 @@
 package validator
 
+import "context"
+
 // Func accepts a FieldLevel interface for all validation needs.
 // Return value should be true when validation succeeds.
 type Func func(fl FieldLevel) bool
@@ -7,3 +9,14 @@ type Func func(fl FieldLevel) bool
 // FuncCtx accepts a context.Context and FieldLevel interface for all validation needs.
 // The return value should be true when validation succeeds.
 type FuncCtx func(ctx context.Context, fl FieldLevel) bool
+
+// wrapFunc wraps normal Func makes it compatible with FuncCtx.
+func wrapFunc(fn Func) FuncCtx {
+	if fn == nil {
+		return nil
+	}
+
+	return func(ctx context.Context, fl FieldLevel) bool {
+		return fn(fl)
+	}
+}
