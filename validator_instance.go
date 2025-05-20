b0aa2f97e1539245c8a39922d464856c3f6d7242
pchchv <ipchchv@gmail.com>
2025-05-20 12:24:00 +0300

    Add CustomTypeFunc type

--- validator_instance.go
+++ validator_instance.go
@@ -4,3 +4,8 @@ import "reflect"
 
 // TagNameFunc allows for adding of a custom tag name parser.
 type TagNameFunc func(field reflect.StructField) string
+
+// CustomTypeFunc overrides or adds custom field type handler functions
+// field = field type value to return a value to validate Valuer example from sql drive
+// see https://golang.org/src/database/sql/driver/types.go?s=1210:1293#L29
+type CustomTypeFunc func(field reflect.Value) interface{}
