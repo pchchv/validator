938e58a4cfc238ebfc23fd40f988f1690cc2c9ec
pchchv <ipchchv@gmail.com>
2025-05-20 12:25:59 +0300

    Add FilterFunc type

--- validator_instance.go
+++ validator_instance.go
@@ -9,3 +9,7 @@ type TagNameFunc func(field reflect.StructField) string
 // field = field type value to return a value to validate Valuer example from sql drive
 // see https://golang.org/src/database/sql/driver/types.go?s=1210:1293#L29
 type CustomTypeFunc func(field reflect.Value) interface{}
+
+// FilterFunc is the type used to filter fields using the StructFiltered(...) function.
+// Return true causes the field to be filtered/skipped on validation.
+type FilterFunc func(ns []byte) bool
