663eeb99208f70ddd6e9236f7dbbf714bf240c9e
pchchv <ipchchv@gmail.com>
2025-05-20 12:34:42 +0300

    Add FuncCtx type

--- baked_in.go
+++ baked_in.go
@@ -3,3 +3,7 @@ package validator
 // Func accepts a FieldLevel interface for all validation needs.
 // Return value should be true when validation succeeds.
 type Func func(fl FieldLevel) bool
+
+// FuncCtx accepts a context.Context and FieldLevel interface for all validation needs.
+// The return value should be true when validation succeeds.
+type FuncCtx func(ctx context.Context, fl FieldLevel) bool
