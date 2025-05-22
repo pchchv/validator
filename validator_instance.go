4ad99411039ccb6b527ac67093c8433e3e3fadc8
pchchv <ipchchv@gmail.com>
2025-05-22 23:04:44 +0300

    Add a Validate type

--- validator_instance.go
+++ validator_instance.go
@@ -1,6 +1,9 @@
 package validator
 
-import "reflect"
+import (
+	"reflect"
+	"sync"
+)
 
 // TagNameFunc allows for adding of a custom tag name parser.
 type TagNameFunc func(field reflect.StructField) string
@@ -13,6 +16,24 @@ type CustomTypeFunc func(field reflect.Value) interface{}
 // FilterFunc is the type used to filter fields using the StructFiltered(...) function.
 // Return true causes the field to be filtered/skipped on validation.
 type FilterFunc func(ns []byte) bool
+
+// Validate contains the validator settings and cache.
+type Validate struct {
+	tagName                string
+	pool                   *sync.Pool
+	tagNameFunc            TagNameFunc
+	structLevelFuncs       map[reflect.Type]StructLevelFuncCtx
+	customFuncs            map[reflect.Type]CustomTypeFunc
+	aliases                map[string]string
+	validations            map[string]internalValidationFuncWrapper
+	rules                  map[reflect.Type]map[string]string
+	tagCache               *tagCache
+	structCache            *structCache
+	hasCustomFuncs         bool
+	hasTagNameFunc         bool
+	requiredStructEnabled  bool
+	privateFieldValidation bool
+}
 
 type internalValidationFuncWrapper struct {
 	fn                 FuncCtx
