1cc9698079325eeffc7f568b519600732d8002f2
pchchv <ipchchv@gmail.com>
2025-05-22 23:35:22 +0300

    Add validate type

--- validator.go
+++ validator.go
@@ -1,2 +1,24 @@
 package validator
 
+import "reflect"
+
+type validate struct {
+	v              *Validate
+	top            reflect.Value
+	ns             []byte
+	actualNs       []byte
+	errs           ValidationErrors
+	includeExclude map[string]struct{} // reset only if StructPartial or StructExcept are called, no need otherwise
+	ffn            FilterFunc
+	slflParent     reflect.Value // StructLevel & FieldLevel
+	slCurrent      reflect.Value // StructLevel & FieldLevel
+	flField        reflect.Value // StructLevel & FieldLevel
+	cf             *cField       // StructLevel & FieldLevel
+	ct             *cTag         // StructLevel & FieldLevel
+	misc           []byte        // misc reusable
+	str1           string        // misc reusable
+	str2           string        // misc reusable
+	fldIsPointer   bool          // StructLevel & FieldLevel
+	isPartial      bool
+	hasExcludes    bool
+}
