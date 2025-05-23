88a058ae617780b2ceac0f59c692afeccf6be648
pchchv <ipchchv@gmail.com>
2025-05-23 20:56:20 +0300

    Implement getStructFieldOKInternal method for validate

--- util.go
+++ util.go
@@ -1,6 +1,10 @@
 package validator
 
-import "reflect"
+import (
+	"reflect"
+	"strconv"
+	"strings"
+)
 
 // extractTypeInternal gets the actual underlying type of field value.
 // It will dive into pointers, customTypes and return you the underlying value and it's kind.
@@ -34,4 +38,144 @@ BEGIN:
 		}
 		return current, current.Kind(), nullable
 	}
+}
+
+// getStructFieldOKInternal traverses a struct to retrieve a specific field denoted by the
+// provided namespace and returns the field,
+// field kind and whether is was successful in retrieving the field at all.
+//
+// NOTE: when not successful ok will be false,
+// this can happen when a nested struct is nil and so the field could not be retrieved because it didn't exist.
+func (v *validate) getStructFieldOKInternal(val reflect.Value, namespace string) (current reflect.Value, kind reflect.Kind, nullable bool, found bool) {
+BEGIN:
+	current, kind, nullable = v.ExtractType(val)
+	if kind == reflect.Invalid {
+		return
+	}
+
+	if namespace == "" {
+		found = true
+		return
+	}
+
+	switch kind {
+	case reflect.Ptr, reflect.Interface:
+		return
+	case reflect.Struct:
+		var ns string
+		typ := current.Type()
+		fld := namespace
+		if !typ.ConvertibleTo(timeType) {
+			idx := strings.Index(namespace, namespaceSeparator)
+			if idx != -1 {
+				fld = namespace[:idx]
+				ns = namespace[idx+1:]
+			} else {
+				ns = ""
+			}
+
+			bracketIdx := strings.Index(fld, leftBracket)
+			if bracketIdx != -1 {
+				fld = fld[:bracketIdx]
+
+				ns = namespace[bracketIdx:]
+			}
+
+			val = current.FieldByName(fld)
+			namespace = ns
+			goto BEGIN
+		}
+
+	case reflect.Array, reflect.Slice:
+		idx := strings.Index(namespace, leftBracket)
+		idx2 := strings.Index(namespace, rightBracket)
+		arrIdx, _ := strconv.Atoi(namespace[idx+1 : idx2])
+		if arrIdx >= current.Len() {
+			return
+		}
+
+		startIdx := idx2 + 1
+		if startIdx < len(namespace) {
+			if namespace[startIdx:startIdx+1] == namespaceSeparator {
+				startIdx++
+			}
+		}
+
+		val = current.Index(arrIdx)
+		namespace = namespace[startIdx:]
+		goto BEGIN
+	case reflect.Map:
+		idx := strings.Index(namespace, leftBracket) + 1
+		idx2 := strings.Index(namespace, rightBracket)
+		endIdx := idx2
+		if endIdx+1 < len(namespace) {
+			if namespace[endIdx+1:endIdx+2] == namespaceSeparator {
+				endIdx++
+			}
+		}
+
+		key := namespace[idx:idx2]
+		switch current.Type().Key().Kind() {
+		case reflect.Int:
+			i, _ := strconv.Atoi(key)
+			val = current.MapIndex(reflect.ValueOf(i))
+			namespace = namespace[endIdx+1:]
+		case reflect.Int8:
+			i, _ := strconv.ParseInt(key, 10, 8)
+			val = current.MapIndex(reflect.ValueOf(int8(i)))
+			namespace = namespace[endIdx+1:]
+		case reflect.Int16:
+			i, _ := strconv.ParseInt(key, 10, 16)
+			val = current.MapIndex(reflect.ValueOf(int16(i)))
+			namespace = namespace[endIdx+1:]
+		case reflect.Int32:
+			i, _ := strconv.ParseInt(key, 10, 32)
+			val = current.MapIndex(reflect.ValueOf(int32(i)))
+			namespace = namespace[endIdx+1:]
+		case reflect.Int64:
+			i, _ := strconv.ParseInt(key, 10, 64)
+			val = current.MapIndex(reflect.ValueOf(i))
+			namespace = namespace[endIdx+1:]
+		case reflect.Uint:
+			i, _ := strconv.ParseUint(key, 10, 0)
+			val = current.MapIndex(reflect.ValueOf(uint(i)))
+			namespace = namespace[endIdx+1:]
+		case reflect.Uint8:
+			i, _ := strconv.ParseUint(key, 10, 8)
+			val = current.MapIndex(reflect.ValueOf(uint8(i)))
+			namespace = namespace[endIdx+1:]
+		case reflect.Uint16:
+			i, _ := strconv.ParseUint(key, 10, 16)
+			val = current.MapIndex(reflect.ValueOf(uint16(i)))
+			namespace = namespace[endIdx+1:]
+		case reflect.Uint32:
+			i, _ := strconv.ParseUint(key, 10, 32)
+			val = current.MapIndex(reflect.ValueOf(uint32(i)))
+			namespace = namespace[endIdx+1:]
+		case reflect.Uint64:
+			i, _ := strconv.ParseUint(key, 10, 64)
+			val = current.MapIndex(reflect.ValueOf(i))
+			namespace = namespace[endIdx+1:]
+		case reflect.Float32:
+			f, _ := strconv.ParseFloat(key, 32)
+			val = current.MapIndex(reflect.ValueOf(float32(f)))
+			namespace = namespace[endIdx+1:]
+		case reflect.Float64:
+			f, _ := strconv.ParseFloat(key, 64)
+			val = current.MapIndex(reflect.ValueOf(f))
+			namespace = namespace[endIdx+1:]
+		case reflect.Bool:
+			b, _ := strconv.ParseBool(key)
+			val = current.MapIndex(reflect.ValueOf(b))
+			namespace = namespace[endIdx+1:]
+		default:
+			val = current.MapIndex(reflect.ValueOf(key))
+			namespace = namespace[endIdx+1:]
+		}
+
+		goto BEGIN
+	}
+
+	// if got here there was more namespace, cannot go any deeper
+	panic("Invalid field namespace")
 }
