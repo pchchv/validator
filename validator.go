7f058cb3273ee19b3163120a37a3e52a96528aaf
pchchv <ipchchv@gmail.com>
2025-05-23 20:35:08 +0300

    Implement getValue function

--- validator.go
+++ validator.go
@@ -1,6 +1,9 @@
 package validator
 
-import "reflect"
+import (
+	"reflect"
+	"unsafe"
+)
 
 type validate struct {
 	v              *Validate
@@ -21,4 +24,27 @@ type validate struct {
 	fldIsPointer   bool          // StructLevel & FieldLevel
 	isPartial      bool
 	hasExcludes    bool
+}
+
+func getValue(val reflect.Value) interface{} {
+	if val.CanInterface() {
+		return val.Interface()
+	}
+
+	if val.CanAddr() {
+		return reflect.NewAt(val.Type(), unsafe.Pointer(val.UnsafeAddr())).Elem().Interface()
+	}
+
+	switch val.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return val.Int()
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return val.Uint()
+	case reflect.Complex64, reflect.Complex128:
+		return val.Complex()
+	case reflect.Float32, reflect.Float64:
+		return val.Float()
+	default:
+		return val.String()
+	}
 }
