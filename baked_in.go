07b8d37b200f82086f0908aef8f903eaf95fc07f
pchchv <ipchchv@gmail.com>
2025-05-26 09:07:15 +0300

    Implement hasMultiByteCharacter and parseOneOfParam funcs

--- baked_in.go
+++ baked_in.go
@@ -3,23 +3,36 @@ package validator
 import (
 	"context"
 	"reflect"
+	"strings"
+	"sync"
 )
 
-var restrictedTags = map[string]struct{}{
-	diveTag:           {},
-	keysTag:           {},
-	endKeysTag:        {},
-	structOnlyTag:     {},
-	omitzero:          {},
-	omitempty:         {},
-	omitnil:           {},
-	skipValidationTag: {},
-	utf8HexComma:      {},
-	utf8Pipe:          {},
-	noStructLevelTag:  {},
-	requiredTag:       {},
-	isdefault:         {},
-}
+var (
+	oneofValsCache       = map[string][]string{}
+	oneofValsCacheRWLock = sync.RWMutex{}
+	restrictedTags       = map[string]struct{}{
+		diveTag:           {},
+		keysTag:           {},
+		endKeysTag:        {},
+		structOnlyTag:     {},
+		omitzero:          {},
+		omitempty:         {},
+		omitnil:           {},
+		skipValidationTag: {},
+		utf8HexComma:      {},
+		utf8Pipe:          {},
+		noStructLevelTag:  {},
+		requiredTag:       {},
+		isdefault:         {},
+	}
+	// bakedInAliases is a default mapping of a single validation tag that
+	// defines a common or complex set of validation(s) to simplify adding validation to structs
+	bakedInAliases = map[string]string{
+		"iscolor":         "hexcolor|rgb|rgba|hsl|hsla",
+		"country_code":    "iso3166_1_alpha2|iso3166_1_alpha3|iso3166_1_alpha_numeric",
+		"eu_country_code": "iso3166_1_alpha2_eu|iso3166_1_alpha3_eu|iso3166_1_alpha_numeric_eu",
+	}
+)
 
 // Func accepts a FieldLevel interface for all validation needs.
 // Return value should be true when validation succeeds.
@@ -38,6 +51,35 @@ func wrapFunc(fn Func) FuncCtx {
 	return func(ctx context.Context, fl FieldLevel) bool {
 		return fn(fl)
 	}
+}
+
+// hasMultiByteCharacter is the validation function for validating if the
+// field's value has a multi byte character.
+func hasMultiByteCharacter(fl FieldLevel) bool {
+	field := fl.Field()
+	if field.Len() == 0 {
+		return true
+	}
+
+	return multibyteRegex().MatchString(field.String())
+}
+
+func parseOneOfParam(s string) []string {
+	oneofValsCacheRWLock.RLock()
+	vals, ok := oneofValsCache[s]
+	oneofValsCacheRWLock.RUnlock()
+	if !ok {
+		oneofValsCacheRWLock.Lock()
+		vals = splitParamsRegex().FindAllString(s, -1)
+		for i := 0; i < len(vals); i++ {
+			vals[i] = strings.ReplaceAll(vals[i], "'", "")
+		}
+
+		oneofValsCache[s] = vals
+		oneofValsCacheRWLock.Unlock()
+	}
+
+	return vals
 }
 
 // hasValue is the validation function for validating if the current field's value is not the default static value.
