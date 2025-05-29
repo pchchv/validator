2b170bf7d7d61259023c7dc9ebed14969b2db75e
pchchv <ipchchv@gmail.com>
2025-05-29 23:14:39 +0300

    Implement WithPrivateFieldValidation option to enable validation for unexported fields

--- options.go
+++ options.go
@@ -3,3 +3,15 @@ package validator
 // Option represents a configurations option to
 // be applied to validator during initialization.
 type Option func(*Validate)
+
+// WithPrivateFieldValidation activates validation for
+// unexported fields using of the `unsafe` package.
+//
+// WARNING: By using this feature,
+// you acknowledge that you are aware of the risks and accept any
+// current or future consequences of using this feature.
+func WithPrivateFieldValidation() Option {
+	return func(v *Validate) {
+		v.privateFieldValidation = true
+	}
+}
