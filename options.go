039ff56f5e8f67270a33c783b1c4631e6bbf4f97
pchchv <ipchchv@gmail.com>
2025-05-29 23:16:10 +0300

    Implement WithRequiredStructEnabled option to enable required tag on non-pointer structs

--- options.go
+++ options.go
@@ -15,3 +15,18 @@ func WithPrivateFieldValidation() Option {
 		v.privateFieldValidation = true
 	}
 }
+
+// WithRequiredStructEnabled enables required tag on
+// non-pointer structs to be applied instead of ignored.
+//
+// This was made opt-in behaviour in order
+// to maintain backward compatibility with the
+// behaviour previous to being able
+// to apply struct level validations on struct fields directly.
+//
+// NOTE: It is recommended to enabled this.
+func WithRequiredStructEnabled() Option {
+	return func(v *Validate) {
+		v.requiredStructEnabled = true
+	}
+}
