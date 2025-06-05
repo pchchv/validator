83fd04ea10c84c650a0f74e84304afb85651a445
pchchv <ipchchv@gmail.com>
2025-06-05 18:44:23 +0300

    Add Address type with required fields for validation

---
+++ examples/simple/main.go
@@ -0,0 +1,9 @@
+package main
+
+// Address houses a users address information.
+type Address struct {
+	Street string `validate:"required"`
+	City   string `validate:"required"`
+	Planet string `validate:"required"`
+	Phone  string `validate:"required"`
+}
