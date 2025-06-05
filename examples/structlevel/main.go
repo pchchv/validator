cb0726fe43f0d06c72aaf3538c14f17c6c5b4f90
pchchv <ipchchv@gmail.com>
2025-06-05 19:17:54 +0300

    Add Address type with validation tags for user information

--- examples/structlevel/main.go
+++ examples/structlevel/main.go
@@ -1,1 +1,9 @@
 package structLevel
+
+// Address houses a users address information.
+type Address struct {
+	Street string `validate:"required"`
+	City   string `validate:"required"`
+	Planet string `validate:"required"`
+	Phone  string `validate:"required"`
+}
