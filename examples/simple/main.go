7791ad8e03a7be92b425c3750f18800bc8589510
pchchv <ipchchv@gmail.com>
2025-06-05 18:51:41 +0300

    Add email validation function and initialize validator instance

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"log"
+
+	"github.com/pchchv/validator"
+)
+
 // Address houses a users address information.
 type Address struct {
 	Street string `validate:"required"`
@@ -17,4 +23,16 @@ type User struct {
 	Gender         string     `validate:"oneof=male female prefer_not_to"`
 	FavouriteColor string     `validate:"iscolor"`                // alias for 'hexcolor|rgb|rgba|hsl|hsla'
 	Addresses      []*Address `validate:"required,dive,required"` // a person can have a home and cottage...
+}
+
+// If a single instance of Validate is used, it caches struct info.
+var validate *validator.Validate
+
+func validateVariable() {
+	myEmail := "ipchchv.gmail.com"
+	if errs := validate.Var(myEmail, "required,email"); errs != nil {
+		log.Println(errs) // output: Key: "" Error:Field validation for "" failed on the "email" tag
+		return
+	}
+	// email ok, move on
 }
