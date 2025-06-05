afae04d2514e6f5ccd79513f3907ef6d80ea9942
pchchv <ipchchv@gmail.com>
2025-06-05 19:49:34 +0300

    Add validationError struct for enhanced error reporting in validation

--- examples/structlevel/main.go
+++ examples/structlevel/main.go
@@ -31,6 +31,20 @@ func (gender Gender) String() string {
 	return terms[gender]
 }
 
+type validationError struct {
+	Namespace       string `json:"namespace"` // can differ when a custom TagNameFunc is registered or
+	Field           string `json:"field"`     // by passing alt name to ReportError like below
+	StructNamespace string `json:"structNamespace"`
+	StructField     string `json:"structField"`
+	Tag             string `json:"tag"`
+	ActualTag       string `json:"actualTag"`
+	Kind            string `json:"kind"`
+	Type            string `json:"type"`
+	Value           string `json:"value"`
+	Param           string `json:"param"`
+	Message         string `json:"message"`
+}
+
 const (
 	Male Gender = iota + 1
 	Female
