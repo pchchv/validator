b5733ecf5e58bd3c3cc19728c4b157402223e11b
pchchv <ipchchv@gmail.com>
2025-06-05 19:02:53 +0300

    Implement ValidateValuer function to improve readability and organization

--- examples/custom_field_types/main.go
+++ examples/custom_field_types/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"database/sql/driver"
+	"reflect"
+)
+
+// ValidateValuer implements validator.CustomTypeFunc.
+func ValidateValuer(field reflect.Value) interface{} {
+	if valuer, ok := field.Interface().(driver.Valuer); ok {
+		if val, err := valuer.Value(); err == nil {
+			return val
+		}
+		// handle the error how you want
+	}
+	return nil
+}
