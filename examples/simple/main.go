3b100272e5b0be949eed424d357326dac72d7546
pchchv <ipchchv@gmail.com>
2025-06-05 18:45:31 +0300

    Add User type with validation rules for user information

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -7,3 +7,14 @@ type Address struct {
 	Planet string `validate:"required"`
 	Phone  string `validate:"required"`
 }
+
+// User contains user information
+type User struct {
+	FirstName      string     `validate:"required"`
+	LastName       string     `validate:"required"`
+	Age            uint8      `validate:"gte=0,lte=130"`
+	Email          string     `validate:"required,email"`
+	Gender         string     `validate:"oneof=male female prefer_not_to"`
+	FavouriteColor string     `validate:"iscolor"`                // alias for 'hexcolor|rgb|rgba|hsl|hsla'
+	Addresses      []*Address `validate:"required,dive,required"` // a person can have a home and cottage...
+}
