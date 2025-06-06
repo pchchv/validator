a542f3d66654f765c36e982b5eb07f274cad8009
pchchv <ipchchv@gmail.com>
2025-06-06 22:10:07 +0300

    Implement Engine and lazyinit methods for defaultValidator

--- examples/ginupgradingoverriding/main.go
+++ examples/ginupgradingoverriding/main.go
@@ -10,3 +10,16 @@ type defaultValidator struct {
 	once     sync.Once
 	validate *validator.Validate
 }
+
+func (v *defaultValidator) Engine() interface{} {
+	v.lazyinit()
+	return v.validate
+}
+
+func (v *defaultValidator) lazyinit() {
+	v.once.Do(func() {
+		v.validate = validator.New()
+		v.validate.SetTagName("binding")
+		// add any custom validations etc. here
+	})
+}
